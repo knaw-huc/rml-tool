258eaaf754091efe3dbb0820a5e73f2861aa0a1a
meindertkroese <>
2020-02-10 13:53:25 +0100

    Ignore unsupported cell types.

--- src/main/java/nl/knaw/huc/rmltool/rml/datasource/excel/ExcelSheetDataSource.java
+++ src/main/java/nl/knaw/huc/rmltool/rml/datasource/excel/ExcelSheetDataSource.java
@@ -8,6 +8,7 @@ import nl.knaw.huc.rmltool.rml.Row;
 import nl.knaw.huc.rmltool.rml.datasource.JoinHandler;
 import nl.knaw.huc.rmltool.rml.datasource.RowFactory;
 import nl.knaw.huc.rmltool.util.Tuple;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import java.util.Iterator;
@@ -17,7 +18,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import static java.util.List.of;
 import static nl.knaw.huc.rmltool.util.Tuple.tuple;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_BOOLEAN;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING;
 
 public class ExcelSheetDataSource implements DataSource {
 
@@ -25,6 +30,7 @@ public class ExcelSheetDataSource implements DataSource {
   private final Sheet sheet;
   private final RowFactory rowFactory;
   private final JoinHandler joinHandler;
+  private static final List<Integer> SUPPORTED_CELL_TYPES = of(CELL_TYPE_NUMERIC, CELL_TYPE_STRING, CELL_TYPE_BOOLEAN);
 
   public ExcelSheetDataSource(Sheet sheet, RowFactory rowFactory) {
     this.sheet = sheet;
@@ -44,10 +50,16 @@ public class ExcelSheetDataSource implements DataSource {
                           final Map<String, String> values = headers
                               .stream()
                               .map(header -> tuple(header, headers.indexOf(header)))
-                              .filter(headerIndex-> row.getCell(headerIndex.getRight()) != null )
+                              .filter(headerIndex -> {
+                                Cell cell = row.getCell(headerIndex.getRight());
+                                    return cell != null &&
+                                        SUPPORTED_CELL_TYPES.contains(cell.getCellType()) &&
+                                        !getCellValue(cell).equals("");
+                                  }
+                              )
                               .map(headerIndex -> tuple(
                                   headerIndex.getLeft(),
-                                  row.getCell(headerIndex.getRight()).getStringCellValue()
+                                  getCellValue(row.getCell(headerIndex.getRight()))
                               )).collect(Collectors.toMap(Tuple::getLeft, Tuple::getRight));
 
 
@@ -55,10 +67,19 @@ public class ExcelSheetDataSource implements DataSource {
                         });
   }
 
+  private String getCellValue(Cell cell) {
+    switch (cell.getCellType()) {
+      case CELL_TYPE_NUMERIC : return "" + cell.getNumericCellValue();
+      case CELL_TYPE_STRING : return cell.getStringCellValue();
+      case CELL_TYPE_BOOLEAN : return "" + cell.getBooleanCellValue();
+      default: return null;
+    }
+  }
+
   private List<String> getHeaders(Iterator<org.apache.poi.ss.usermodel.Row> iterator) {
     final org.apache.poi.ss.usermodel.Row headerRow = iterator.next();
     final List<String> headers = Lists.newArrayList();
-    headerRow.cellIterator().forEachRemaining(cell -> headers.add(cell.getStringCellValue()));
+    headerRow.cellIterator().forEachRemaining(cell -> headers.add(getCellValue(cell)));
     return headers;
   }
 
